b6b32a03916c64c2df3a023dfbe4bfaa24263531
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-19 15:43:14 -0400

    Cards displaying

--- src/components/Beauty.jsx
+++ src/components/Beauty.jsx
@@ -7,23 +7,29 @@ const Beauty = () => {
 const[sparkles, setSparkles] = useState([]);
 
 useEffect(() => {
-let url = `http://makeup-api.herokuapp.com/api/v1/products.json`
-fetch(url).then(response=> response.json()).then(data=> setSparkles(data.sparkles)
+let url = `http://makeup-api.herokuapp.com/api/v1/products.json`;
+fetch(url)
+.then((response)=> response.json())
+.then((data)=> setSparkles(data.sparkles));
 
-}, [])
+}, []);
 
   return (
 <div>
 
 {sparkles.map((products) => {
-  return <Products key={product.id} brand={products.brand} name={products.name} price={products.price} src={product.image_link} alt={product.name}/>
+  return <Products 
+  key={products.id} 
+  brand={products.brand} 
+  name={products.name} 
+  price={products.price} 
+  src={products.image_link} 
+  alt={products.name}
+  />
 })}
 
 </div>
-
-
   )
 }
-
 
 export default Beauty
