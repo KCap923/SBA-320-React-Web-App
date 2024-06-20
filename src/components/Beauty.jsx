b484ab2fc8f1879db1a388b8303289999973e4c0
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-20 15:14:04 -0400

    Style edit

--- src/components/Beauty.jsx
+++ src/components/Beauty.jsx
@@ -7,23 +7,24 @@ const Beauty = () => {
 const[sparkles, setSparkles] = useState([]);
 
 useEffect(() => {
-let url = `http://makeup-api.herokuapp.com/api/v1/products.json`;
+let url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=nyx`;
 fetch(url)
 .then((response)=> response.json())
-.then((data)=> setSparkles(data.sparkles));
+.then((data)=> setSparkles(data));
 
 }, []);
 
   return (
 <div>
 
-{sparkles.map((products) => {
+{sparkles.map((products, index) => {
   return <Products 
-  key={products.id} 
+  key={index} 
   brand={products.brand} 
   name={products.name} 
   price={products.price} 
   src={products.image_link} 
+  url={products.product_link}
   alt={products.name}
   />
 })}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,14 @@
-
-
 const Products = ({brand, name, price, src, url}) => {
   return (
 
     // Cards *************************************************************
-<div className="card bg-warning text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth: "345px" }}>
-  <img src={src} className="card-img-top" alt="..."/>
+<div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth: "345px" }}>
+  <img src={src} style={{height: "180px", width: "320px"}} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{brand}</h5>
     <p className="card-text">{name}</p>
     <p className="card-text">{price}</p>
-
-    <a href={url} className="btn btn-primary"> More Info </a>
+    <a href={url} className="btn btn-warning"> More Info </a>
   </div>
 </div>
   )
@@ -19,7 +16,7 @@ const Products = ({brand, name, price, src, url}) => {
 
 export default Products
 
-
+// conditional rendering 
 
 
 // import { useEffect, useState } from "react";
